ea4addb86b1704fca3938149c065e8de63df295f
eatandcod <95302587+eatandcod@users.noreply.github.com>
2023-09-06 21:36:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js');
-  }
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
+    console.log('Service Worker Registered');
+  });
+}
